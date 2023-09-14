2642f3d21aa396855ca36debfa4a430579fa24fe
dagger <crystson0819@gmail.com>
2023-09-14 18:12:21 +0200

    Initial Project

---
+++ Assets/Scripts/Event Listeners/GameplayMenuEventListener.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayMenuEventListener : MonoBehaviour
+{
+    public Text speedMPH;
+
+    [Serializable]
+    public class CameraSettings
+    {
+        public FollowTarget followCamera;
+
+        [Header("Texts")]
+        public Text xValue;
+        public Text yValue;
+        public Text zValue;
+
+        [Header("Sliders")]
+        public Slider xSlider;
+        public Slider ySlider;
+        public Slider zSlider;
+    }
+
+    public CameraSettings cameraSettings;
+
+    public Slider JDSlider;
+    public Text JDValue;
+
+    public bool isCAPressed = false;
+
+    private void Start()
+    {
+        cameraSettings.xSlider.value = 0;
+        cameraSettings.ySlider.value = 15;
+        cameraSettings.zSlider.value = -20;
+
+        JDSlider.value = 0.5f;
+    }
+
+    public void OnPressedChristAirButton(bool isPressed)
+    {
+        isCAPressed = isPressed;
+    }
+
+    public void OnClickFakieButton()
+    {
+        RefManager.I.playerController.PerFormFakie();
+    }
+
+    public void SetSpeedUI(float speed)
+    {
+        speedMPH.text = "Speed: " + ((int)speed > 70 ? 70 : (int)speed) + " mph";
+    }
+
+    public void OnXVlueChanged(Single value)
+    {
+        cameraSettings.followCamera.offset.x = value;
+        cameraSettings.xValue.text = "" + value;
+    }
+
+    public void OnYVlueChanged(Single value)
+    {
+        cameraSettings.followCamera.offset.y = value;
+        cameraSettings.yValue.text = "" + value;
+    }
+
+    public void OnZVlueChanged(Single value)
+    {
+        cameraSettings.followCamera.offset.z = value;
+        cameraSettings.zValue.text = "" + value;
+    }
+
+    // JD = Jump Distance
+    public void OnJDValueChange(Single value)
+    {
+        RefManager.I.playerController.jumpSettings.max_h_Tojump = 1 - value;
+        JDValue.text = (value).ToString("#.00");
+    }
+}
---
+++ Assets/Scripts/Event Listeners/PlayerAnimEventListener.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimEventListener : MonoBehaviour
+{
+    public bool animCompleted;
+    Transform skateBoard => RefManager.I.skateBoard;
+    public Transform leftFoot;
+    public Transform leftHand;
+
+    private RootMotion.FinalIK.FullBodyBipedIK fullBodyBipedIK;
+    private bool correctRotation;
+
+    private void Start()
+    {
+        fullBodyBipedIK = GetComponent<RootMotion.FinalIK.FullBodyBipedIK>();
+    }
+
+    private void Update()
+    {
+        if (skateBoard.localRotation != Quaternion.Euler(0, 0, 0))
+            skateBoard.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    public void ChangeSkateBoardParent(string parent = "")
+    {
+        switch (parent)
+        {
+            case "Foot":
+                PrintLog("Foot");
+                animCompleted = false;
+                skateBoard.parent = leftFoot;
+                fullBodyBipedIK.enabled = false;
+                correctRotation = true;
+                break;
+            case "Hand":
+                PrintLog("Hand");
+                skateBoard.parent = leftHand;
+                break;
+            case "Completed":
+                //Invoke("AnimationCompleted", 0.2f);
+                AnimationCompleted();
+                break;
+            default:
+                PrintLog("Normal");
+                skateBoard.parent = transform;
+                fullBodyBipedIK.enabled = true;
+                skateBoard.localRotation = Quaternion.Euler(0, 0, 0);
+                correctRotation = false;
+                break;
+        }
+    }
+
+    public void AnimationCompleted()
+    {
+        animCompleted = true;
+    }
+
+    void PrintLog(string log)
+    {
+        Debug.Log("PlayerAnimEventListener: " + log);
+    }
+}
